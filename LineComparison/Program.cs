f6cb4c53463f01e4cc68fc2b572b9674ad765d27
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-07-12 11:44:20 +0530

    [Riya] Add : Implemented Calculate Length

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -7,6 +7,8 @@ namespace LineComparison
         static void Main(string[] args) 
         {
             Console.WriteLine("Welcome to Line Comparison statement");
+            Operation operation = new Operation(2, 1, 4, 3);
+            operation.CalculateLength();
         }
     }
 }
